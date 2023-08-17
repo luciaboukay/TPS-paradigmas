91de2c4c86e65383db4c7ea3c2e0817ce6204d0b
luciaboukay <lboukay@udesa.edu.ar>
2023-08-17 17:25:35 -0300

    cambios

--- Region.hs
+++ Region.hs
@@ -1,9 +1,20 @@
 module Region ( Region, newR, foundR, linkR, tunelR, pathR, linksForR, connectedR, linkedR, delayR, availableCapacityForR, usedCapacityForR )
    where
 
+import Point (Point, newP, difP)
+import City (City, newC, nameC, distanceC)
+import Quality (Quality, delayQ, capacityQ, newQ)
+import Link (Link, newL, linksL, connectsL, capacityL, delayL)
+import Tunel (Tunel, newT, connectsT, usesT, delayT)
+
 data Region = Reg [City] [Link] [Tunel]
+
 newR :: Region
+newR = Reg [] [] []
+
 foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
+foundR (Reg cities _ _) city = city:cities
+
 linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
 tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
 connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
